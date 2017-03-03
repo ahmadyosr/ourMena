a1ac5761c060f6854e590189a2c46f4692af1224
hussamEddinAlhendi <housam35@gmail.com>
2017-03-03 13:28:28 +0200

    Add allStudent route

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,17 +3,25 @@ var helpers = require('./helpers.js'); // our custom middleware
 module.exports = function (app, db) {
 
   app.post('/addStudent', function (req, res, next) {
-    console.log(req.body)
     db.collection('students').insert({
       name: req.body.name,
+      age: req.body.age,
       major: req.body.major,
       gpa: req.body.gpa || null,
       video: req.body.video || null,
       pic: req.body.pic || null,
       cost: req.body.cost || null,
-      story: req.body.story || null
+      story: req.body.story || null,
+      numberOfSponsers: req.body.numberOfSponsers || null,
+      progress: req.body.progress || null
     }).then(result => {
-      console.log(result)
+      res.json(result.ops);
+    })
+  })
+
+  app.get('/allStudent', function (req, res, next) {
+    db.collection('students').find().toArray().then(results => {
+      res.json(results);
     })
   })
   
